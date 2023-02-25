be730270ec7ea110956bd560abb1901534154fbf
ououssama <oussamaouaissa1@gmail.com>
2023-02-25 15:25:00 +0100

    add base url

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,6 +1,6 @@
 export default function Contact() {
     return (
-        <form method="POST" action="#" name="login" netlify>
+        <form method="POST" name="login" data-netlify="true">
             <input type="hidden" name="form-name" value="login"/>
             <input type="email" placeholder="Email" name="email"/>
             <input type="password" placeholder="Password" name="password" />
