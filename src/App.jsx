72c1a0d75880f293449ac9c09973f113e7c5f735
ououssama <oussamaouaissa1@gmail.com>
2023-02-25 16:06:59 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@
 function App() {
 
   return (
-    <form name='login' method="POST" netlify onSubmit={(e) => e.preventDefault()}>
+    <form name='login' method="POST" data-netlify="true" onSubmit="submit">
       <input type='hidden' name='form-name' value='login' />
-      <input type='email' placeholder='Email' name='email' />
-      <input type='password' placeholder='Password' name='password' />
+      <label htmlFor="email">Email :<input type='email' placeholder='Email' name='email' /></label>
+      <label htmlFor="password">Password :<input type='password' placeholder='Password' name='password' /></label>
       <button type='submit'>login</button>
     </form>
   );
