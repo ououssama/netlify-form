1e51838ed387cdd36e03b13b7c9d603b0e81c712
ououssama <oussamaouaissa1@gmail.com>
2023-02-25 15:12:26 +0100

    new update

---
+++ src/Contact.jsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+    return (
+        <form method="POST" name="login" netlify>
+            <input type="hidden" name="form-name" value="login"/>
+            <input type="email" placeholder="Email" name="email"/>
+            <input type="password" placeholder="Password" name="password" />
+            <button type="submit">login</button>
+        </form>
+    )
+}
